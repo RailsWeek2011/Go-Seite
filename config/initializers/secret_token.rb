ef5036c7ee3dca110bb67359d68889b3ada934b4
Hermann Sutter <hermann.sutter@mni.th-mittelhessen.de>
2011-07-21 11:43:51 +0200

    Add skeletal structure.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GoSeite::Application.config.secret_token = '94b048e2427c47a3c5c6e999fc46f91798ca40350cd615aa396a9b9b3babf573531b68bee48d34880aad0f728f9bdc53c49cb2f620992a9cfccc08026fcadae2'
